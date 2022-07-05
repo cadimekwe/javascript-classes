6f85811097efcd5a63a4b22479af263591c0e226
cadimekwe <chidimmadimekwe@gmail.com>
2022-07-05 14:26:49 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+// prints Hello World
+let number = 100;
+number = number + 10;
+number += 10;
+console.log(number);
+console.log('hi' + 'ya');
+console.log('front  ' + 'space');
+console.log('My name is chidimma'.length);
+console.log('hello'.toUpperCase());
+console.log('Chidims'.startsWith('p'));
+let name = 'Ebuks the daddy';
+console.log(name);
+name = 'local government';
+console.log(name);
