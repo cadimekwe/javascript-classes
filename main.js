2ce589a0dc9ca866c717cd4ee9675e8e93c7764d
cadimekwe <chidimmadimekwe@gmail.com>
2022-07-05 21:34:52 +0100

    number exercises in javascript

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ let name = 'Ebuks the daddy';
 console.log(name);
 name = 'local government';
 console.log(name);
+
+//Number Exercise 1 
+let age = 27;
+console.log(age);
+
+//Number Exercise 2
+let numberOfRabbits = 2;
+numberOfRabbits = 3 * 2;
+console.log(numberOfRabbits);
+
+//Number Exercise 3
+let bodmasExample = 3;
+bodmasExample = bodmasExample + 5 / 6;
+console.log(bodmasExample);
+
+//Number Exercise 4
+let randomNumber = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber);
