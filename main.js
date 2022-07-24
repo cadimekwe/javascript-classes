1b61f8e14eb1dd636ae87249b662cbda48dfdc0c
cadimekwe <chidimmadimekwe@gmail.com>
2022-07-24 20:05:17 +0100

    addedstrings methods

--- main.js
+++ main.js
@@ -31,3 +31,60 @@ console.log(bodmasExample);
 //Number Exercise 4
 let randomNumber = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber);
+
+let firstName = 'CHIDIMMA ';
+console.log(firstName.toLocaleLowerCase());
+let lastName = 'adimekwe';
+console.log(lastName.toUpperCase());
+console.log(firstName + lastName.toUpperCase());
+
+
+// concatination
+let message = 'Welcome ';
+let message2 = 'Home';
+let addition = message + message2;
+console.log(addition);
+
+//length property
+let messageLength = firstName.length;
+console.log(messageLength);
+
+//template literals
+let email = "chidimmadimekwe@gmail.com";
+console.log(`my email address is ${email}`);
+
+//include- string method to check if a value exist in a text
+const country = 'united kingdom';
+// const doesItExist = country.includes('united');
+// Replace- replaces a text in a string
+const replaceText = country.replace('kingdom', 'states');
+console.log(replaceText);
+console.log(country);
+
+// String Exercise 1
+let myCountry = 'Nigeria';
+console.log(myCountry);
+
+// String Exercise 2
+console.log(myCountry.length);
+
+// String Exercise 3
+const favouriteFood = 'chidimma';
+console.log(favouriteFood.toUpperCase());
+
+// string exercise 4
+console.log(favouriteFood.toLocaleLowerCase());
+
+// string exercise 5
+let middleName = 'ursula';
+let myAge = 27;
+let myMessage = 'what is your name? ' + 'hello, my name is ' + middleName + ' and i am ' + myAge  + ' years old';
+console.log(myMessage);
+
+// string exercise 6
+const firstName2 = 'chisomnazu';
+const lastName2 = 'adimekwe';
+console.log(`my sisters name is ${firstName2} ${lastName2}`);
+console.log(`Welcome ${firstName2} ${lastName2} How can i help you?`);
+
+// 
